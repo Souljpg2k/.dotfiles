5f0554ea25ced0b91f97c37489577baa94388ca0
arx <crystalsoul2k@gmail.com>
2026-04-05 00:52:49 +0700

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting
+
+starship init fish | source
+
+alias ff='fastfetch'
+alias h='nvim $HOME/.config/hypr/hyprland.conf'
+alias a='nvim $HOME/.config/hypr/hyprland/animations.conf'
+alias c='nvim $HOME/.config/hypr/hyprland/colors.conf'
+alias k='nvim $HOME/.config/hypr/hyprland/keybinds.conf'
+alias r='nvim $HOME/.config/hypr/hyprland/rules.conf'
+alias f='nvim $HOME/.config/fish/config.fish'
